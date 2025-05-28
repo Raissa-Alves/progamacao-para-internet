f035013c53d5b5cde4c4e52e68b2a7a9fd7b7b76
Raissa-Alves <raissaalvees99@gmail.com>
2025-05-28 10:22:46 -0300

    Adicionando mais um algoritmo de ordenacao,insertion sort

--- algoritmosdOrdenacao.py
+++ algoritmosdOrdenacao.py
@@ -6,3 +6,15 @@ def bubble_sort(array:list):
         tempo = array[index]
         array[index] = array[index+1]
         array[index + 1] = tempo
+
+# modificação feita no clone na maquina local
+
+def insertion_sort(array:list):
+     for index in range(1,len(array)):
+        valorAtual = array[index]
+        posicao = index
+        while posicao >0 and array[posicao-1]>valorAtual:
+          array[posicao] = array[posicao-1]
+          posicao = posicao-1
+
+        array[posicao]= valorAtual
